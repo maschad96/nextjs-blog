d311b710930b622f970862af26d175ec63759175
Matthew Schad <matt@exactstrategy.com>
2021-04-11 01:32:00 -0400

    add netlify cms

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/global.css'
 
-const App = ({ Component, pageProps }) => (
-	<Component {...pageProps} />
-)
+const App = ({ Component, pageProps }) => {
+	return (
+		<>
+			<Component {...pageProps} />
+			<script src="/scripts/netlify-identity.js" />
+		</>
+	)
+}
 export default App;
