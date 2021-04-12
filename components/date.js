f625ec35e5dece8c657fa47d793a29d00ef21f13
Matthew Schad <matt@exactstrategy.com>
2021-04-11 22:47:14 -0400

    use short date

--- components/date.js
+++ components/date.js
@@ -3,5 +3,5 @@ import { parseISO, format } from 'date-fns';
 export default function Date({ dateString }) {
 	const date = parseISO(dateString);
 	const dateTime = format(date, 'P');
-	return <time dateTime={dateTime}>{format(dateTime, 'LLLL d, yyyy')}</time>
+	return <time dateTime={dateTime}>{date}</time>
 }
