d4684d6260ba3dc06fbf3d27cb9a0742458e021d
Matthew Schad <matt@exactstrategy.com>
2021-04-11 22:47:30 -0400

    typo

--- components/date.js
+++ components/date.js
@@ -3,5 +3,5 @@ import { parseISO, format } from 'date-fns';
 export default function Date({ dateString }) {
 	const date = parseISO(dateString);
 	const dateTime = format(date, 'P');
-	return <time dateTime={dateTime}>{date}</time>
+	return <time dateTime={dateTime}>{dateTime}</time>
 }
