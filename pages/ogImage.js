5ba4ebd629e88e21e95950ed648cfd6c1e90cc9e
Matthew Schad <matt@exactstrategy.com>
2021-04-19 23:56:20 -0400

    add tailwind, add ogImage component, playwright image screenshot process

---
+++ pages/ogImage.js
@@ -0,0 +1,77 @@
+import { useRouter } from "next/router";
+import { GoogleFonts } from "next-google-fonts";
+
+function getFontSize(length) {
+	if (length >= 50) {
+		return `text-5xl`;
+	}
+
+	if (length >= 32) {
+		return `text-7xl`;
+	}
+
+	return `text-9xl`;
+}
+
+// Example URL: http://localhost:3000/ogImage?title=Hello%20mein%20Name%20ist%20Florian!&url=https://mattjschad.com/hello-world
+const ogImage = () => {
+	const router = useRouter();
+
+	const searchParams = new URLSearchParams(router.asPath.split(/\?/)[1]);
+	const link = searchParams.get("url");
+
+	if (!link) return null;
+
+	const linkURL = new URL(link);
+	const title = searchParams.get("title");
+	const date = searchParams.get("date");
+	const readTime = searchParams.get("readTime");
+
+	return (
+		<>
+			<GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter&family=Roboto:wght@400;700&display=swap" />
+			<div
+				className="relative flex flex-col justify-between p-16 text-gray-100 bg-gray-900 shadow-md"
+				style={{ width: 1200, height: 630 }}
+			>
+				<div className="max-w-screen-lg space-y-2">
+					{date && readTime && <p className="text-3xl font-semibold text-gray-600 font-source-sans-pro">
+						<span>{date}</span> — <span>{readTime}</span>
+					</p>}
+					<h1
+						className={`${getFontSize(
+							title.length
+						)} font-bold text-gray-100 font-source-sans-pro`}
+					>
+						{title}
+					</h1>
+				</div>
+				<div className="flex justify-between">
+					<div className="flex items-center space-x-6">
+						<img
+							src='images/profile.jpeg'
+							alt="Matthew Schad"
+							className="flex-none w-32 h-32 border-4 border-gray-200 rounded-full"
+						/>
+						<div className="flex flex-col gap">
+							<p className="mb-1 text-3xl font-semibold text-gray-200 font-source-sans-pro">
+								Matthew Schad
+              				</p>
+							<p className="text-2xl font-semibold tracking-wide text-indigo-400 font-open-sans">
+								mattjschad.com<span className="path">{linkURL.pathname}</span>
+							</p>
+							<p
+								className="text-2xl font-semibold tracking-wide font-source-sans-pro"
+								style={{ color: "#1D9BF0" }}
+							>
+								twitter.com/mattjschad
+              				</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default ogImage;
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,18 @@
 import Layout from '../../components/layout';
 import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import getOgImage from '../../lib/getOgImage';
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css';
 
-export default function Post({ postData }) {
+export default function Post({ ogImage, postData }) {
 	return (
 		<Layout>
 			<Head>
 				<title>{postData.title}</title>
+				<meta name="description" content={postData.description} />
+				<meta name="og:title" content={postData.title} />
+				<meta property="og:image" content={ogImage} key="og-image" />
 			</Head>
 			<article>
 				<h1 className={utilStyles.headingXl}>{postData.title}</h1>
@@ -33,10 +37,14 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
 	// Fetch necessary data for the blog post using params.id
-	const postData = await getPostData(params.id)
+	const postData = await getPostData(params.id);
+	const ogImage = await getOgImage(
+		`/ogImage?title=${encodeURIComponent(postData.title)}&url=${process.env.DEPLOY_URL}/${params.id}`
+	);
 	return {
 		props: {
-			postData
+			postData,
+			ogImage
 		}
 	}
 }
